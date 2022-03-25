f0ec0bbc89959d4338cdb84fb60dae9eb59210d1
Jumbef <Jumbef@users.noreply.github.com>
2022-03-25 16:26:16 +0100

    Delete runwapttasks.sh

--- runwapttasks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo -u wapt PYTHONPATH=/opt/wapt PYTHONHOME=/opt/wapt /opt/wapt/bin/python /opt/wapt/waptserver/wapthuey.py waptenterprise.waptserver.wsus_tasks.huey -w 2 -k thread $@
-
