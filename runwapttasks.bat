dda74e7c600bd0f4bb8d1abd57b0ff6ba9c2e8ea
Jumbef <Jumbef@users.noreply.github.com>
2022-03-25 16:26:09 +0100

    Delete runwapttasks.bat

--- runwapttasks.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Run waptservice python script in wapt virtualenv for debugging purpose
-net stop wapttasks
-pushd %~dp0
-SET VIRTUAL_ENV=%CD%
-SET PYTHONHOME=%VIRTUAL_ENV%
-"%PYTHONHOME%\waptpython.exe" "%PYTHONHOME%\waptserver\wapthuey.py" waptenterprise.waptserver.wsus_tasks.huey -w 2 -k thread %*
-popd
